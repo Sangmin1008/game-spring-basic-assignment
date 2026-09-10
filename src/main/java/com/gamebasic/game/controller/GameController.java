b26c4e3646049a9258706aec7c33997a04d4d509
Sangmin1008 <sangmin11119@gmail.com>
2026-09-10 14:41:29 +0900

    feat : GameService 빈 등록

--- src/main/java/com/gamebasic/game/controller/GameController.java
+++ src/main/java/com/gamebasic/game/controller/GameController.java
@@ -7,6 +7,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
